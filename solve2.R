a234bd6051408dead6dee75c13aedfc65f8c8618
mauricio <33768118+mauriciogregory@users.noreply.github.com>
2020-11-23 16:20:03 -0300

    Update solve2.R

--- solve2.R
+++ solve2.R
@@ -1,10 +1,14 @@
 makeVector <- function(x = numeric()) {
+    
     m <- NULL
+    
     set <- function(y) {
         x <<- y
         m <<- NULL
     }
+    
     get <- function() x
+    
     setmean <- function(mean) m <<- mean
     getmean <- function() m
     list(set = set, get = get,
@@ -13,11 +17,14 @@ makeVector <- function(x = numeric()) {
 }
 
 cachemean <- function(x, ...) {
+    
     m <- x$getmean()
+    
     if(!is.null(m)) {
-        message("getting cached data")
+        message("getting data")
         return(m)
     }
+    
     data <- x$get()
     m <- mean(data, ...)
     x$setmean(m)
